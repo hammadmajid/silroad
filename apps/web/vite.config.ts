f7b6667fb9ff78ba1d3ea2a64b94d7a4aba777cf
Hammad Majid <hammadmajid@proton.me>
2025-07-15 19:45:25 +0000

    fix: deprecated import and configure tanstackRotuer to match with biome config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,19 @@
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+	plugins: [
+		tanstackRouter({
+			autoCodeSplitting: true,
+			quoteStyle: 'single',
+			addExtensions: true,
+			semicolons: false,
+		}),
+		viteReact(),
+	],
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, './src'),
