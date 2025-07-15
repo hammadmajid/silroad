5f165388fd91346360a4ca4e5eb19f2572fd08e2
Hammad Majid <hammadmajid@proton.me>
2025-07-15 21:53:55 +0000

    feat: test App to use tRPC for data fetching and mutation

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,35 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
 import '../App.css'
+
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { trpc } from '@/utils/trpc'
 
 export const Route = createFileRoute('/')({
 	component: App,
 })
 
 function App() {
+	const testQuery = useQuery(trpc.test.testQuery.queryOptions())
+	const testMutation = useMutation(trpc.test.testMutation.mutationOptions())
+
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/routes/index.tsx</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-				<a
-					className='App-link'
-					href='https://tanstack.com'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn TanStack
-				</a>
+			<header>
+				<h1>{testQuery.data?.message}</h1>
+				<button onClick={() => testMutation.mutate()}>
+					{testMutation.status}
+				</button>
 			</header>
 		</div>
 	)
