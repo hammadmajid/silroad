e97a9862538f3f9666dcd84861ba13ab4d9aed2d
Hammad Majid <hammadmajid@proton.me>
2025-08-03 02:32:43 +0500

    config: drizzle-kit to authenticate with D1 with token

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,24 @@
 import { defineConfig } from 'drizzle-kit';
 
+const accountId = process.env.CLOUDFLARE_ACCOUNT_ID;
+const token = process.env.CLOUDFLARE_TOKEN;
+
+if (!accountId) {
+	throw new Error("CLOUDFLARE_ACCOUNT_ID is undefined");
+}
+
+if (!token) {
+	throw new Error("CLOUDFLARE_TOKEN is undefined");
+}
+
 export default defineConfig({
 	dialect: 'sqlite',
-	schema: './src/db/schema.ts'
+	schema: './src/db/schema.ts',
+	driver: 'd1-http',
+	dbCredentials: {
+		accountId,
+		databaseId: "593db8b3-fa2d-452b-92db-ac7c7d3f4659",
+		token,
+	},
+	tablesFilter: ["!_cf_KV"]
 });
