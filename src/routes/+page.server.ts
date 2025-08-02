d9bb61c69cb83331fe23b528e9d74b7b781713fe
Hammad Majid <hammadmajid@proton.me>
2025-08-03 03:15:31 +0500

    chore: delete example

--- src/routes/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { users } from "../db/schema";
-import { getDb } from "$lib/db";
-import type { PageServerLoad } from "./$types";
-
-export const load: PageServerLoad = async ({ platform }) => {
-    const db = getDb(platform);
-    const userList = await db.select().from(users);
-    return { users: userList };
-};
