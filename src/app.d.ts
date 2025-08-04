4d26ef88a09506f34409b338ede0b88d5588ad51
Hammad Majid <hammadmajid@proton.me>
2025-08-04 12:45:42 +0000

    feat: strongly type user locals

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,21 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
-	namespace App {
+    namespace App {
         interface Platform {
             env: Env
             cf: CfProperties
             ctx: ExecutionContext
         }
+        interface Locals {
+            user: {
+                id: string,
+                email: string,
+                name: string,
+                image: string | null,
+            }
+        }
     }
 }
 
-export {};
+export { };
