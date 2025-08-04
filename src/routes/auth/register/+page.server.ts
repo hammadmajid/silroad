7b0ab57345383eda650c39afb975d28ed81588c0
Hammad Majid <hammadmajid@proton.me>
2025-08-04 12:08:56 +0000

    feat: implement user registration by adding password hashing and database integration

--- src/routes/auth/register/+page.server.ts
+++ src/routes/auth/register/+page.server.ts
@@ -2,27 +2,49 @@ import type { PageServerLoad } from "./$types";
 import { schema } from "./schema";
 import { superValidate, message } from "sveltekit-superforms";
 import { zod4 } from "sveltekit-superforms/adapters";
-import { fail } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
+import { getDb } from "$lib/db"
+import { users } from "$lib/db/schema";
+import { eq } from "drizzle-orm";
+import { generateSalt, hashPassword } from "$lib/utils/crypto";
 
 export const load: PageServerLoad = async () => {
     return {
-        // @ts-expect-error: https://github.com/ciscoheat/sveltekit-superforms/issues/626
         form: await superValidate(zod4(schema)),
     };
 };
 
 export const actions = {
-    default: async ({ request }) => {
-        // @ts-expect-error: https://github.com/ciscoheat/sveltekit-superforms/issues/626
+    default: async ({ request, platform }) => {
         const form = await superValidate(request, zod4(schema));
+        const db = getDb(platform);
 
         if (!form.valid) {
             return fail(400, { form });
         }
 
-        // TODO: store user in db
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+        const { firstName, lastName, email, password } = form.data;
 
-        return message(form, 'User created successfully!');
+        // hash password here to prevent timing attack
+        const salt = generateSalt();
+        const hashedPassword = await hashPassword(password, salt);
+
+        const exists = await db.select().from(users).where(eq(users.email, email));
+
+        if (exists.length > 0) {
+            return message(form, 'Failed to create user');
+        }
+
+        const [user] = await db.insert(users).values({
+            name: `${firstName} ${lastName}`,
+            email: email,
+            password: hashedPassword,
+            hash: salt,
+        }).returning()
+
+        // TODO: create session in db
+        // TODO: cache session in KV
+
+        throw redirect(301, "/settings/profile");
     }
 }
