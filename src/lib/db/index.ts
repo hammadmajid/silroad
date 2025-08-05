bc736f024a8557a5e0e11b556e8c9eb987b9d9c9
Hammad Majid <hammadmajid@proton.me>
2025-08-05 21:31:53 +0500

    feat: add function to safely get KV ref

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -16,3 +16,19 @@ export function getDb(platform: App.Platform | undefined) {
 
 	return drizzle(platform.env.DB);
 }
+
+export function getKV(platform: App.Platform | undefined) {
+	if (!platform) {
+		throw error(500, 'Platform not available');
+	}
+
+	if (!platform.env) {
+		throw error(500, 'Environment not available');
+	}
+
+	if (!platform.env.KV) {
+		throw error(500, 'KV not available');
+	}
+
+	return platform.env.KV;
+}
