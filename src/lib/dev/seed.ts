427a2b09e33637888e331c9ff301eb086c716367
Hammad Majid <hammadmajid@proton.me>
2025-08-07 20:23:15 +0500

    feat: udpate seed logic to include organizations and related tables

--- src/lib/dev/seed.ts
+++ src/lib/dev/seed.ts
@@ -10,9 +10,32 @@ const hashedPassword = await hashPassword(password, salt);
 
 export async function seedDatabase(
   platform: App.Platform | undefined,
-  opts: { users: number; sessions: number }
+  opts: { users: number; sessions: number; organizations: number }
 ) {
   const db = getDb(platform);
+
+  await db.delete(schema.users).run();
+  await db.delete(schema.organizations).run();
+  await db.delete(schema.sessions).run();
+  await db.delete(schema.organizationMembers).run();
+  await db.delete(schema.verificationTokens).run();
+
+  const organizations: typeof schema.organizations.$inferInsert[] = [];
+
+  for (let i = 0; i < opts.organizations; i++) {
+    const id = crypto.randomUUID();
+    const name = faker.company.name();
+    organizations.push({
+      id,
+      name: name,
+      slug: faker.helpers.slugify(name.toLowerCase()),
+      description: faker.company.catchPhrase(),
+      avatar: faker.image.avatar(),
+      backgroundImage: faker.image.urlPicsumPhotos(),
+    });
+  }
+
+  await db.insert(schema.organizations).values(organizations).run();
 
   const users: typeof schema.users.$inferInsert[] = [];
 
@@ -22,6 +45,8 @@ export async function seedDatabase(
     const email = faker.internet.email();
     const image = faker.image.avatarGitHub();
 
+    const org = i < opts.organizations ? organizations[i] : undefined;
+
     users.push({
       id,
       name,
@@ -29,10 +54,23 @@ export async function seedDatabase(
       image,
       password: hashedPassword,
       salt,
+      organizationId: org?.id || null,
     });
   }
 
   await db.insert(schema.users).values(users).run();
+
+  const members: typeof schema.organizationMembers.$inferInsert[] = [];
+
+  for (const user of users) {
+    if (!user.organizationId) continue;
+    members.push({
+      userId: user.id!,
+      organizationId: user.organizationId,
+    });
+  }
+
+  await db.insert(schema.organizationMembers).values(members).run();
 
   const sessions: typeof schema.sessions.$inferInsert[] = [];
 
