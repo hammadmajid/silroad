53147b1e379f69f581dc88fdc3cec895e91adaa9
Hammad Majid <hammadmajid@proton.me>
2025-08-09 00:00:40 +0500

    fix: allow speical chars in password and reduce error length

--- src/routes/(auth)/register/schema.ts
+++ src/routes/(auth)/register/schema.ts
@@ -1,16 +1,16 @@
 import { z } from 'zod/v4';
 
 export const schema = z.object({
-	firstName: z.string().min(1, 'Please enter a valid name'),
-	lastName: z.string().min(1, 'Please enter a valid name'),
-	email: z.email('Please enter a valid email.'),
+	firstName: z.string().min(1, 'Enter a valid name'),
+	lastName: z.string().min(1, 'Enter a valid name'),
+	email: z.email('Enter a valid email.'),
 	password: z
 		.string()
 		.regex(
-			/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-			'Password must be at least 8 characters long and contain both letters and numbers.'
+			/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{8,}$/,
+			'Password must be 8+ chars, with letters, numbers and special.'
 		),
 	agree: z.boolean().refine((val) => val === true, {
-		message: 'You must agree to the Terms and Privacy Policy'
+		message: 'You must agree to the terms'
 	})
 });
