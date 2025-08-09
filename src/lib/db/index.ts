5fbf61ece6f910d9e223f0e55b1b6e7fc6bb7693
Hammad Majid <hammadmajid@proton.me>
2025-08-09 20:09:33 +0500

    feat: add function to safely get logger

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -32,3 +32,19 @@ export function getKV(platform: App.Platform | undefined) {
 
 	return platform.env.KV;
 }
+
+export function getLogger(platform: App.Platform | undefined) {
+	if (!platform) {
+		throw error(500, 'Platform not available');
+	}
+
+	if (!platform.env) {
+		throw error(500, 'Environment not available');
+	}
+
+	if (!platform.env.LOGGER) {
+		throw error(500, 'KV not available');
+	}
+
+	return platform.env.LOGGER;
+}
