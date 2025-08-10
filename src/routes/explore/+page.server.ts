e417ee2f0b9ae698903e1b9d85d39cb160718167
Hammad Majid <hammadmajid@proton.me>
2025-08-10 18:30:54 +0500

    fix: allow unauthenticated user on /explore route

--- src/routes/explore/+page.server.ts
+++ src/routes/explore/+page.server.ts
@@ -6,10 +6,6 @@ import { redirect } from '@sveltejs/kit';
 export const load: PageServerLoad = async ({ locals, platform }) => {
 	const db = getDb(platform);
 
-	if (!locals.user) {
-		throw redirect(303, '/login');
-	}
-
 	// Return promises instead of awaited results to allow component-level loading states
 	return {
 		events: db.select().from(events).limit(5),
