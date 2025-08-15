85b540e67fa5778c038ad7e798dcf2257dba52c5
Hammad Majid <hammadmajid@proton.me>
2025-08-15 05:42:48 +0500

    refactor: move all tests to tests/ dir

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	webServer: {
-		command: 'pnpm run preview',
+		command: 'pnpm run build && pnpm run preview',
 		port: 8787
 	},
-	testDir: 'e2e'
+	testDir: 'tests/e2e'
 });
