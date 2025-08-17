98654313252b262e50e4ae0a5701954ac3736eed
Hammad Majid <hammadmajid@proton.me>
2025-08-18 03:55:56 +0500

    feat: use dummy keys in non-production env

--- src/lib/utils/env.ts
+++ src/lib/utils/env.ts
@@ -3,6 +3,12 @@ export function isProduction(platform: App.Platform | undefined): boolean {
 }
 
 export function getPublicTurnstileKey(platform: App.Platform | undefined) {
+	if (!isProduction(platform)) {
+		// Turnstile dummy test key, always passes
+		// See: https://developers.cloudflare.com/turnstile/troubleshooting/testing/
+		return "1x00000000000000000000AA";
+	}
+
 	const key = platform?.env.PUBLIC_TURNSTILE_KEY;
 
 	if (!key) {
@@ -13,6 +19,12 @@ export function getPublicTurnstileKey(platform: App.Platform | undefined) {
 }
 
 export function getSecretTurnstileKey(platform: App.Platform | undefined) {
+	if (!isProduction(platform)) {
+		// Turnstile dummy test key, always passes
+		// See: https://developers.cloudflare.com/turnstile/troubleshooting/testing/
+		return "1x0000000000000000000000000000000AA";
+	}
+
 	const key = platform?.env.SECRET_TURNSTILE_KEY;
 
 	if (!key) {
