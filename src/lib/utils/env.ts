ea395ce3d1e3b491edda4fa6b88e086a519a64e7
Hammad Majid <hammadmajid@proton.me>
2025-08-18 02:07:36 +0500

    feat: add functions to safely get turnstile credentials

--- src/lib/utils/env.ts
+++ src/lib/utils/env.ts
@@ -1,3 +1,23 @@
 export function isProduction(platform: App.Platform | undefined): boolean {
 	return platform?.env.NODE_ENV === 'production';
 }
+
+export function getPublicTurnstileKey(platform: App.Platform | undefined) {
+	const key = platform?.env.PUBLIC_TURNSTILE_KEY;
+
+	if (!key) {
+		throw 'Turnstile public key not found';
+	}
+
+	return key;
+}
+
+export function getSecretTurnstileKey(platform: App.Platform | undefined) {
+	const key = platform?.env.SECRET_TURNSTILE_KEY;
+
+	if (!key) {
+		throw 'Turnstile secret key not found';
+	}
+
+	return key;
+}
