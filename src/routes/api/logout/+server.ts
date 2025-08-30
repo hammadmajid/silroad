bce4c75675ac40b0688a0404250b5a439a8b0b82
Hammad Majid <hammadmajid@proton.me>
2025-08-30 17:49:10 +0500

    fix: ensure session deletion from KV store during logout

--- src/routes/api/logout/+server.ts
+++ src/routes/api/logout/+server.ts
@@ -1,9 +1,17 @@
-import { SESSION_COOKIE_NAME } from '$lib/repos/session';
+import { SESSION_COOKIE_NAME, SessionRepo } from '$lib/repos/session';
+import { getKV } from '$lib/db';
 
-export const POST = async ({ cookies }) => {
-	// TODO: delete session from DB and KV
+export const POST = async ({ cookies, platform }) => {
+	const token = cookies.get(SESSION_COOKIE_NAME);
+	const kv = getKV(platform);
+
+	if (token && platform) {
+		platform.ctx.waitUntil(
+			kv.delete(token)
+		);
+	}
 
 	cookies.delete(SESSION_COOKIE_NAME, { path: '/' });
 
-	return new Response();
+	return new Response(null, { status: 204 });
 };
